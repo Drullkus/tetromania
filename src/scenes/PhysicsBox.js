fb23a6b5826f110effd72f816e4c3368b31a576d
Drullkus <5010174+Drullkus@users.noreply.github.com>
2026-02-06 19:41:41 -0800

    Surprisingly less jank than I anticipated?

--- src/scenes/PhysicsBox.js
+++ src/scenes/PhysicsBox.js
@@ -7,8 +7,6 @@ class PhysicsBox extends Phaser.Scene {
     create() {
         const tetrominoCollisions = this.cache.json.get('tetromino_collision');
 
-        //this.matter.add.mouseSpring(); // Allow mouse to pick up and drag objects
-
         this.tetrominoes = tetrominoNames.map((tetrominoName, index) => {
             return this[tetrominoName] = this.matter.add.sprite(150 * (index % 4) + 75, 150 * Math.floor(index / 4) + 75, tetrominoName, 0, {
                 shape: tetrominoCollisions[tetrominoName]
@@ -22,11 +20,18 @@ class PhysicsBox extends Phaser.Scene {
         this.controlUi = this.game.scene.getScene('controlInterfaceScene');
     }
 
-    update() {
-        const { deltaX, deltaY } = this.controlUi.getControlDelta();
-        const speed = 0.05;
+    update(_time, deltaMillis) {
+        const deltaSeconds = deltaMillis * 0.001;
+        const speed = 8 * deltaSeconds;
+        const motionDelta = this.controlUi.getControlDelta();
+
+        const deltaX = motionDelta.deltaX * speed;
+        const deltaY = motionDelta.deltaY * speed;
+        
         this.tetrominoes.forEach(tetromino => {
-            tetromino.setVelocity(deltaX * speed, deltaY * speed);
+            // tetromino.setVelocity(deltaX * speed, deltaY * speed);
+            tetromino.x += deltaX * speed;
+            tetromino.y += deltaY * speed;
         });
     }
 }
