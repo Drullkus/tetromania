b14fff129c30dc66dd828a31efd35d1843707eb6
Drullkus <5010174+Drullkus@users.noreply.github.com>
2026-02-16 23:51:30 -0800

    Add particles

--- src/scenes/Initialize.js
+++ src/scenes/Initialize.js
@@ -15,6 +15,11 @@ class Initialize extends Phaser.Scene {
         this.load.spritesheet('asteroid_small', './assets/objects/asteroid_small.png', { frameWidth: 64 });
         this.load.spritesheet('asteroid_medium', './assets/objects/asteroid_medium.png', { frameWidth: 256 });
 
+        const particleAnimationConfig = { frameWidth: 16 };
+        this.load.spritesheet('debris', './assets/objects/debris.png', particleAnimationConfig);
+        this.load.spritesheet('explosion', './assets/objects/explosion.png', particleAnimationConfig);
+        this.load.spritesheet('fire', './assets/objects/fire.png', particleAnimationConfig);
+
         this.load.json('tetromino_collision', './assets/collision/tetromino.json');
         this.load.json('technology_collision', './assets/collision/technology.json');
     }
@@ -40,6 +45,20 @@ class Initialize extends Phaser.Scene {
 
         window.partCentroids = partCentroids;
 
+        this.createAnimations();
+
         this.scene.start('orbitScene');
+    }
+
+    createAnimations() {
+        ['debris', 'explosion','fire'].forEach(name => 
+            [0, 1, 2, 3].forEach((columnIndex, _index, list) => this.anims.create({
+                key: `${name}-${columnIndex}`,
+                frames: this.anims.generateFrameNumbers(name, {
+                    frames: [1, 0, 1, 2, 3].map(rowIndex => rowIndex * list.length + columnIndex)
+                }),
+                frameRate: 15
+            }))
+        );
     }
 }
