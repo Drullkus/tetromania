72a281af355026fe15a3bca9226b1b8b2f6666d0
Drullkus <5010174+Drullkus@users.noreply.github.com>
2026-02-16 23:23:01 -0800

    Relocate blocks to objects directory

--- src/scenes/Initialize.js
+++ src/scenes/Initialize.js
@@ -7,13 +7,12 @@ class Initialize extends Phaser.Scene {
         this.load.image('controller-circle', './assets/ui/controller-circle.png');
 
         tetrominoNames.forEach(tetrominoName => {
-            this.load.image(tetrominoName, `./assets/blocks/${tetrominoName}.png`);
+            this.load.image(tetrominoName, `./assets/objects/${tetrominoName}.png`);
         });
 
-        this.load.image('thruster', './assets/blocks/thruster.png')
+        this.load.image('thruster', './assets/objects/thruster.png');
 
         this.load.json('tetromino_collision', './assets/collision/tetromino.json');
-
         this.load.json('technology_collision', './assets/collision/technology.json');
     }
 
